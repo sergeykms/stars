cde0e0330ac50fa799a6874b44f01305af74727c
Sergey <serpp@me.com>
2024-07-09 16:40:06 +1000

    09_07_2024__16_39

---
+++ resources/views/components/elements/field_text.blade.php
@@ -0,0 +1,6 @@
+@props(['content' => '$content'])
+
+<div class="field">
+    <p class="field__name">{{ $content[0] }}</p>
+    <p class="field__text">{{ $content[1] }}</p>
+</div>
--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -2,32 +2,44 @@
     <x-home>
         <div class="film">
             <x-elements.title>Показ фильма</x-elements.title>
-            <a class="btn btn-primary app__button" href="{{ route('films.edit', $film->id) }}">Редактировать</a>
+            <a class="btn btn-primary app__button" href="{{ route('films.edit', $film->id) }}"
+               style="margin-bottom: 1rem">Редактировать</a>
 
             <div class="row g-3">
-                <p><span>Наименование: </span>{{ $film->name }}</p>
-                <p><span>Цикл: </span>{{ $film->cycle }}</p>
-                <p><span>Эпизод: </span>{{ $film->episode }}</p>
-                <p><span>Краткое содержание: </span>{{ $film->description }}</p>
-                <p><span>Режиссер: </span>{{ $film->director }}</p>
-                <p><span>Музыка: </span>{{ $film->music }}</p>
-                <p><span>Продюсер: </span>{{ $film->producer }}</p>
-                <p><span>Сценарий: </span>{{ $film->script }}</p>
-                <p><span>Бюджет: </span>{{ $film->budget }}</p>
-                <p><span>Продолжительность, мин.: </span>{{ $film->timing }}</p>
-                <p><span>Дата выхода: </span>{{ $film->release_date }}</p>
+                {{--                <x-elements.title>ggg</x-elements.title>--}}
+
+                <x-elements.field_text :content="['Наименование: ', $film->name]"/>
+                <x-elements.field_text :content="['Цикл: ', $film->cycle]"/>
+                <x-elements.field_text :content="['Эпизод: ', $film->episode]"/>
+                <x-elements.field_text :content="['Пролог: ', $film->description]"/>
+                <x-elements.field_text :content="['Режиссер: ', $film->director]"/>
+                <x-elements.field_text :content="['Музыка: ', $film->music]"/>
+                <x-elements.field_text :content="['Продюсер: ', $film->producer]"/>
+                <x-elements.field_text :content="['Сценарий: ', $film->script]"/>
+                <x-elements.field_text :content="['Бюджет: ', $film->budget]"/>
+                <x-elements.field_text :content="['Продолжительность, мин.: ', $film->timing]"/>
+                <x-elements.field_text :content="['Дата выхода: ', $film->release_date]"/>
+                <x-elements.field_text :content="['Бюджет: ', $film->budget]"/>
                 <h2>История создания: </h2>
                 @php
                     $film_about = explode("\n", $film->about);
                     foreach ($film_about as $about) {
-                        echo "<p>$about</p>";
+                        if(strlen($about) > 50) {
+                            echo "<p class='info__text'>$about</p>";
+                        } else {
+                            echo "<h3 class='info__subtitle'>$about</h3>";
+                        }
                     }
                 @endphp
                 <h2>Краткое содержание: </h2>
                 @php
                     $film_content = explode("\n", $film->content);
                     foreach ($film_content as $content) {
-                        echo "<p>$content</p>";
+                        if(strlen($content) > 50) {
+                            echo "<p class='info__text'>$content</p>";
+                        } else {
+                            echo "<h3 class='info__subtitle''>$content</h3>";
+                        }
                     }
                 @endphp
             </div>
