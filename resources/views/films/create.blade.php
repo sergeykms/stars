ddd7d5188413f0a86828974855f49bdb64c90c0f
Sergey <serpp@me.com>
2024-07-09 10:37:39 +1000

    09_07_2024__10_37

--- resources/views/films/create.blade.php
+++ resources/views/films/create.blade.php
@@ -1,72 +1,74 @@
 <x-layout>
     <x-home>
-        <x-elements.title>Добавление нового фильма</x-elements.title>
+        <div class="film">
+            <x-elements.title>Добавление нового фильма</x-elements.title>
 
-        <form class="row g-3" method="POST" action="{{ route('films.store') }}">
-            @csrf
-            <div class="col-12">
-                <label for="name" class="form-label">Наименование</label>
-                <input type="text" class="form-control" id="name" name="name">
-            </div>
-            <div class="col-md-6">
-                <label for="cycle" class="form-label">Цикл</label>
-                <input type="text" class="form-control" id="cycle" name="cycle">
-            </div>
-            <div class="col-md-6">
-                <label for="episode" class="form-label">Эпизод</label>
-                <input type="number" class="form-control" id="episode" name="episode">
-            </div>
-            <div class="col-12">
-                <label for="description" class="form-label">Краткое содержание</label>
-                <textarea class="form-control" name="description" id="description" rows="4"></textarea>
-            </div>
-            <div class="col-md-6">
-                <label for="director" class="form-label">Режиссер</label>
-                <input type="text" class="form-control" id="director" name="director">
-            </div>
-            <div class="col-md-6">
-                <label for="music" class="form-label">Музыка</label>
-                <input type="text" class="form-control" id="music" name="music">
-            </div>
-            <div class="col-md-6">
-                <label for="producer" class="form-label">Продюсер</label>
-                <input type="text" class="form-control" id="producer" name="producer">
-            </div>
-            <div class="col-md-6">
-                <label for="script" class="form-label">Сценарий</label>
-                <input type="text" class="form-control" id="script" name="script">
-            </div>
-            <div class="col-md-6">
-                <label for="budget" class="form-label">Бюджет</label>
-                <input type="number" class="form-control" id="budget" name="budget">
-            </div>
-            <div class="col-md-6">
-                <label for="timing" class="form-label">Продолжительность, мин.</label>
-                <input type="number" class="form-control" id="timing" name="timing">
-            </div>
-            <div class="col-md-6">
-                <label for="release_date" class="form-label">Дата выхода</label>
-                <input type="date" class="form-control" id="release_date" name="release_date">
-            </div>
-            <div class="col-12">
-                <label for="avatar" class="form-label">Изображение большое</label>
-                <input type="text" class="form-control" id="avatar" name="avatar">
-            </div>
-            <div class="col-12">
-                <label for="avatar_min" class="form-label">Изображение маленькое</label>
-                <input type="text" class="form-control" id="avatar_min" name="avatar_min">
-            </div>
-            <div class="col-12">
-                <label for="about" class="form-label">История создания</label>
-                <textarea class="form-control" name="about" id="about" rows="20"></textarea>
-            </div>
-            <div class="col-12">
-                <label for="content" class="form-label">Краткий пересказ сюжета</label>
-                <textarea class="form-control" name="content" id="content" rows="20"></textarea>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary app__button">Сохранить</button>
-            </div>
-        </form>
+            <form class="row g-3" method="POST" action="{{ route('films.store') }}">
+                @csrf
+                <div class="col-12">
+                    <label for="name" class="form-label">Наименование</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+                <div class="col-md-6">
+                    <label for="cycle" class="form-label">Цикл</label>
+                    <input type="text" class="form-control" id="cycle" name="cycle">
+                </div>
+                <div class="col-md-6">
+                    <label for="episode" class="form-label">Эпизод</label>
+                    <input type="number" class="form-control" id="episode" name="episode">
+                </div>
+                <div class="col-12">
+                    <label for="description" class="form-label">Краткое содержание</label>
+                    <textarea class="form-control" name="description" id="description" rows="4"></textarea>
+                </div>
+                <div class="col-md-6">
+                    <label for="director" class="form-label">Режиссер</label>
+                    <input type="text" class="form-control" id="director" name="director">
+                </div>
+                <div class="col-md-6">
+                    <label for="music" class="form-label">Музыка</label>
+                    <input type="text" class="form-control" id="music" name="music">
+                </div>
+                <div class="col-md-6">
+                    <label for="producer" class="form-label">Продюсер</label>
+                    <input type="text" class="form-control" id="producer" name="producer">
+                </div>
+                <div class="col-md-6">
+                    <label for="script" class="form-label">Сценарий</label>
+                    <input type="text" class="form-control" id="script" name="script">
+                </div>
+                <div class="col-md-6">
+                    <label for="budget" class="form-label">Бюджет</label>
+                    <input type="number" class="form-control" id="budget" name="budget">
+                </div>
+                <div class="col-md-6">
+                    <label for="timing" class="form-label">Продолжительность, мин.</label>
+                    <input type="number" class="form-control" id="timing" name="timing">
+                </div>
+                <div class="col-md-6">
+                    <label for="release_date" class="form-label">Дата выхода</label>
+                    <input type="date" class="form-control" id="release_date" name="release_date">
+                </div>
+                <div class="col-12">
+                    <label for="avatar" class="form-label">Изображение большое</label>
+                    <input type="text" class="form-control" id="avatar" name="avatar">
+                </div>
+                <div class="col-12">
+                    <label for="avatar_min" class="form-label">Изображение маленькое</label>
+                    <input type="text" class="form-control" id="avatar_min" name="avatar_min">
+                </div>
+                <div class="col-12">
+                    <label for="about" class="form-label">История создания</label>
+                    <textarea class="form-control" name="about" id="about" rows="20"></textarea>
+                </div>
+                <div class="col-12">
+                    <label for="content" class="form-label">Краткий пересказ сюжета</label>
+                    <textarea class="form-control" name="content" id="content" rows="20"></textarea>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary app__button">Сохранить</button>
+                </div>
+            </form>
+        </div>
     </x-home>
 </x-layout>
--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -1,7 +1,8 @@
 <x-layout>
     <x-home>
-        <x-elements.title>Показ фильма</x-elements.title>
-        <a class="btn btn-primary app__button" href="{{ route('films.edit', $film->id) }}">Редактировать</a>
-
+        <div class="film">
+            <x-elements.title>Показ фильма</x-elements.title>
+            <a class="btn btn-primary app__button" href="{{ route('films.edit', $film->id) }}">Редактировать</a>
+        </div>
     </x-home>
 </x-layout>
